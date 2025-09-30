d0af1dceb9378a2c53e4e45d2c4b8b6ea16fa9bd
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-10-01 05:02:15 +0530

    app

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,25 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const controller= require("./controller");
 
+const app = express();
+
+// middleware
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// sample route
+app.get("/users", (req, res) => {
+    controller.getUsers((users) => {
+  res.send(users);
+});
+});
+app.get('/user', (req, res) => {
+    const id = req.query.id;
+    controller.getUserById(id, user => {
+        res.send(user);
+    });
+});
 
 module.exports = app;
