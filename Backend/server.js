b342d08cd05d016182ff29a126c3ee64a9983025
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-10-01 05:02:28 +0530

    server

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,7 @@
-const app = reqire('./app');
-const port = '3001';
-const host = '127.0.0.1';
-
+const app = require("./app"); // ✅ fixed typo
+const port = 3001; // ✅ make it a number
+const host = "127.0.0.1";
 
 const server = app.listen(port, host, () => {
-    console.log(`Node server is listening to ${server.address().port}`)
+  console.log(`Node server is listening at http://${host}:${port}`);
 });
