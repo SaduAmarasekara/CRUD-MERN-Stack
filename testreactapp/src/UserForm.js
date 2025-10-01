aa26fd10bd6a0d0706df1410cb850b083a72aea2
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-10-01 22:40:37 +0530

    userForm

--- testreactapp/src/UserForm.js
+++ testreactapp/src/UserForm.js
@@ -1,100 +1,118 @@
 import { Button, Grid, Typography } from '@mui/material';
 import { Input } from '@mui/material';
 import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 
 
 
-const UsersForms = props => {
 
-    const [id, setId] = useState(0)
-    const [name, setName] = useState('')
-    
-    return (
-      <Grid
-        container
-        spacing={2}
-        sx={{
-          backgroundColor: "#ffffff",
-          marginBottom: "30px",
-          display: "block",
-        }}
-      >
-        <Grid item xs={12}>
-          <Typography component={"h1"} sx={{ color: "#000000" }}>
-            User Form
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={6} sx={{ display: "flex" }}>
-          <Typography
-            component={"label"}
-            htmlFor="id"
-            sx={{
-              color: "#000000",
-              marginRight: "20px",
-              fontSize: "16px",
-              width: "100px",
-              display: "block",
-            }}
-          >
-            ID
-          </Typography>
-          <Input
-            type="number"
-            id="id"
-            name="id"
-            sx={{
-              width: "400px",
-            }}
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-          />
-        </Grid>
 
-        <Grid item xs={12} sm={6} sx={{ display: "flex" }}>
-          <Typography
-            component={"label"}
-            htmlFor="id"
-            sx={{
-              color: "#000000",
-              marginRight: "20px",
-              fontSize: "16px",
-              width: "100px",
-              display: "block",
-            }}
-          >
-            Name
-          </Typography>
-          <Input
-            type="text"
-            id="name"
-            name="name"
-            sx={{
-              width: "400px",
-            }}
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </Grid>
+const UsersForms = ({ addUser, updateUser, submitted, data ,isEdit}) => {
+  const [id, setId] = useState(0);
+  const [name, setName] = useState("");
 
-        <Button
+  useEffect(() => {
+    if (!submitted) {
+      setId(0);
+      setName("");
+    }
+  }, [submitted]);
+
+  useEffect(() => {
+    if (data?.id && data.id !== 0) {
+      setId(data.id);
+      setName(data.name);
+      
+    }
+  }, [data]);
+
+  return (
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        backgroundColor: "#ffffff",
+        marginBottom: "30px",
+        display: "block",
+      }}
+    >
+      <Grid item xs={12}>
+        <Typography component={"h1"} sx={{ color: "#000000" }}>
+          User Form
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6} sx={{ display: "flex" }}>
+        <Typography
+          component={"label"}
+          htmlFor="id"
           sx={{
-            margin: "auto",
-            marginBottom: "20px",
-            backgroundColor: "#00c6e6",
             color: "#000000",
-            marginLeft: "15px",
-            marginTop: "20px",
-            "&:hover": {
-              opacity: "0.7",
-              backgroundColor: "#00c6e6",
-            },
+            marginRight: "20px",
+            fontSize: "16px",
+            width: "100px",
+            display: "block",
           }}
         >
-          Add
-        </Button>
+          ID
+        </Typography>
+        <Input
+          type="number"
+          id="id"
+          name="id"
+          sx={{
+            width: "400px",
+          }}
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
       </Grid>
-    );
-    
-}
+
+      <Grid item xs={12} sm={6} sx={{ display: "flex" }}>
+        <Typography
+          component={"label"}
+          htmlFor="id"
+          sx={{
+            color: "#000000",
+            marginRight: "20px",
+            fontSize: "16px",
+            width: "100px",
+            display: "block",
+          }}
+        >
+          Name
+        </Typography>
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          sx={{
+            width: "400px",
+          }}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </Grid>
+
+      <Button
+        sx={{
+          margin: "auto",
+          marginBottom: "20px",
+          backgroundColor: "#00c6e6",
+          color: "#000000",
+          marginLeft: "15px",
+          marginTop: "20px",
+          "&:hover": {
+            opacity: "0.7",
+            backgroundColor: "#00c6e6",
+          },
+        }}
+        onClick={() => isEdit ? updateUser({ id, name }) :addUser({ id, name })}
+      >
+        {
+          isEdit ? "Update " : "Add "}
+      </Button>
+    </Grid>
+  );
+};
 export default UsersForms;
