32b92b16c00360e0c5c65fbbc0cf8a134ea18b43
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-10-01 20:17:01 +0530

    controller

--- Backend/controller.js
+++ Backend/controller.js
@@ -1,16 +1,52 @@
-const users = [
-  { id: 1, name: "John Doe" },
-  { id: 2, name: "Jane Smith" },
-];
+const User = require('./model');
 
-const getUsers = (cb) => {
-  cb(users);
+const getUsers = (req, res, next) => {
+  User.find()
+    .then(response => {
+      res.json({response})
+    })
+  .catch (error => {
+    res.json({ error });
+  })
 };
 
-const getUserById = (id, cb) => {
-  const user = users.find((user) => user.id === Number(id)); // ✅ fix here
-  cb(user);
+const addUsers = (req, res, next) => {
+  const user = new User({
+    id: req.body.id,
+    name: req.body.name,
+  });
+  user
+    .save()
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({error});
+    });
+ 
 };
+
+const updateUser = (req, res, next) => {
+  const {id, name} = req.body;
+  User.updateOne({ id: id }, { $set: { name: name } })
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({error});
+    });
+};
+
+const deleteUser = (req, res, next) => {
+  const id = req.body.id;
+  User.deleteOne({ id: id })
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ error });
+    });
+}
 
 exports.getUsers = getUsers;
 exports.getUserById = getUserById;
