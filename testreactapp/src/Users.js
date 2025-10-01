c0580ab5a392b15f3affdc6d5e0a3ab8ce19619f
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-10-01 22:40:43 +0530

    user

--- testreactapp/src/Users.js
+++ testreactapp/src/Users.js
@@ -1,31 +1,110 @@
 import { Box } from '@mui/material';
 import UserForm from './UserForm';
 import UsersTable from './UsersTable';
+import Axios from "axios"; 
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
-const users = [
-    {
-        id: 1,
-        name: 'John Doe'
-    },
 
-    {
-        id: 2,
-        name: 'Jane Smith',
-    }
-    
-    
-];
+
+
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+    const [submitted, setSubmitted] = useState(false);
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [isEdit, setIsEdit] = useState(false);
+
+
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+
+    const getUsers = () => {
+        Axios.get('http://localhost:3001/api/users')
+            .then((response) => {
+              setUsers(response?.data?.response || [])
+            })
+            .catch(error => {
+            console.error('Axios Eror:', error);
+        })
+    }
+
+    const addUser = (data) => {
+        setSubmitted(true);
+        
+      const payload = {
+        id: data.id,
+        name: data.name,
+        }
+        
+
+      Axios.post("http://localhost:3001/api/createuser", payload)
+        .then(() => {
+          getUsers();
+          setSubmitted(false);
+        })
+        .catch((error) => {
+          console.error("Axios Eror:", error);
+        });
+    }
+
+    const updateUser = (data) => {
+        setSubmitted(true);
+        const payload = {
+            id: data.id,
+            name: data.name
+        }
+        Axios.post("http://localhost:3001/api/updateuser", payload)
+            .then(() => {
+                getUsers();
+                setSubmitted(false);
+                isEdit(false);
+            })
+            .catch((error) => {
+                console.error("Axios Eror:", error);
+            });
+    }
+
+    const deleteUser = (data) => {
+Axios
+  .post("http://localhost:3001/api/deleteuser", { id: data.id })
+
+  .then(() => {
+    getUsers();
+  })
+  .catch((error) => {
+    console.error("Axios Eror:", error);
+  });
+    }
+
+
+
     return (
-        <Box
-            sx={{
-                width: 'calc(100% - 100px)',
-                margin: 'auto',
-                marginTop: '100px',
-        }}>
-        <UserForm />
-            <UsersTable rows={users } />
+      <Box
+        sx={{
+          width: "calc(100% - 100px)",
+          margin: "auto",
+          marginTop: "100px",
+        }}
+      >
+            <UserForm
+                addUser={addUser}
+                updateUser={updateUser}
+                submitted={submitted}
+                data={selectedUser}
+                isEdit={isEdit}
+            />
+            <UsersTable rows={users}
+                selectedUser={data => {
+                    setSelectedUser(data);
+                    setIsEdit(true);
+                }}
+                    deleteUser={data => window.confirm("Are you sure you want to delete this user?") && deleteUser(data)
+                 }
+            />
       </Box>
     );
 }
