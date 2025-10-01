8c3722181537c874d448f54a9d73a6f0dd074063
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-10-01 22:40:55 +0530

    usersTable

--- testreactapp/src/UsersTable.js
+++ testreactapp/src/UsersTable.js
@@ -1,8 +1,11 @@
-import { Paper, TableContainer, TableHead, TableRow ,TableCell, TableBody, Button,Table } from '@mui/material';
+import { Paper, TableContainer, TableHead, TableRow, TableCell, TableBody, Button, Table } from '@mui/material';
+import React from 'react';
 
 
-const Userstable = ({ rows }) => {
-    return(
+
+
+const Userstable = ({ rows, selectedUser, deleteUser }) => {
+  return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
@@ -11,38 +14,58 @@ const Userstable = ({ rows }) => {
             <TableCell>Name</TableCell>
             <TableCell>Actions</TableCell>
           </TableRow>
-            </TableHead>
-            <TableBody>
-                {
-                   rows.length > 0 ? rows.map(row => (
-                        <TableRow key={row.id} sx={{'&:last-child td, &:last-child th': { border: 0 }}}>
-                            <TableCell component='th' scope='row'>{row.id}</TableCell>
-                            <TableCell component='th' scope='row'>{row.name}</TableCell>
-                            <TableCell>
-                <Button
-                    sx={{ margin: '0px 10px' }}
-                    onClick={() => { }}
-                >
+        </TableHead>
+        <TableBody>
+          {rows.length > 0 ? (
+            rows.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell>
+                  <Button
+                    sx={{ margin: "0px 10px" }}
+                    onClick={() =>
+                      selectedUser({
+                        id: row.id,
+                        name: row.name,
+                      })
+                    }
+                  >
                     Update
-                </Button>
-                 <Button
-                    sx={{ margin: '0px 10px' }}
-                    onClick={() => { }}
-                >
+                  </Button>
+                  <Button
+                    sx={{ margin: "0px 10px" }}
+                    onClick={() =>
+                     deleteUser({
+                        id: row.id,
+                      
+                      })
+                    }
+                  >
                     Delete
-                </Button>
-            </TableCell>
-        </TableRow>
-        )): (
-        <TableRow  sx={{ '&:last-child td, &:last-child th': { border: 0 }}}>
-            <TableCell component='th' scope='row' colSpan={3} >No Records Found</TableCell>
-        </TableRow>
-        )
-    }
-            </TableBody>
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row" colSpan={3}>
+                No Records Found
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
       </Table>
-        </TableContainer>
-    )
-
-}
+    </TableContainer>
+  );
+};
 export default Userstable;
